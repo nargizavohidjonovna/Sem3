9f9d4d5fad12e7020e898404081ca7e5988ec819
Nargiza <nargiza9772@gmail.com>
2023-09-28 12:34:29 +0500

    this is my conversion

---
+++ main.py
@@ -0,0 +1,9 @@
+#task5
+decimal_number = input("Please, type decimal")
+decimal_to_binary = bin(int(decimal_number))
+#print("This is binary" + decimal_to_binary)
+
+decimal_number = input("Please, type decimal")
+decimal_to_oct = oct(int(decimal_number))
+print("This is binary" + decimal_to_binary)
+
